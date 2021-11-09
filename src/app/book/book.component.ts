971741c5674c30e0a94f6a26c0a87f5e0806bbb6
aiday-mar <aidaymarlenkyzy@gmail.com>
2021-11-09 14:28:39 +0100

    Error in the booking but find a way to solve the problem

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -16,8 +16,33 @@ interface Flight {
 })
 export class BookComponent implements OnInit {
   flights: Flight[] = flightsData;
+  from: string = "";
+  to: string = "";
 
   constructor() {}
+
+  onChangeFrom(event) {
+    this.from = event;
+    console.log(this.to);
+    this.flights = flightsData.filter(function (entry) {
+      if (this.to !== "") {
+        return entry.from.includes(event) && entry.to.includes(this.to);
+      } else {
+        return entry.from.includes(event);
+      }
+    });
+  }
+
+  onChangeTo(event) {
+    this.to = event;
+    this.flights = flightsData.filter(function (entry) {
+      if (this.from !== "") {
+        return entry.to.includes(event) && entry.from.includes(this.from);
+      } else {
+        return entry.to.includes(event);
+      }
+    });
+  }
 
   onClickSubmit(data) {
     this.flights = flightsData.filter(function (entry) {
