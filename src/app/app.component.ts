40939c5c852a5dbeb1a1469274ad8d11a09e6a5e
aiday-mar <aidaymarlenkyzy@gmail.com>
2021-11-10 11:04:40 +0100

    I added a number which tells us the number of elements added to the shopping cart, need to make the colors persist despite changing the tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { BookingService } from './services/booking.service';
+
+
+interface Flight {
+  id: String;
+  from: String;
+  to: String;
+  departure: String;
+  arrival: String;
+  price: String;
+}
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular';
+  flightsToPay: Flight[];
+
+  constructor(private bookingService: BookingService) {
+    this.flightsToPay = bookingService.flightsToPay;
+  }
 }
