7e97f258be9a24914ad2079322df3cc3694f5e42
aiday-mar <aidaymarlenkyzy@gmail.com>
2021-11-10 14:30:41 +0100

    Added a payment portal with stripe. It is to note that the payment portal is going to default to the language of the browser used.

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -17,10 +17,50 @@ interface Flight {
 })
 export class PaymentComponent implements OnInit {
   flightsToPay: Flight[];
+  paymentHandler:any = null;
 
   constructor(private bookingService: BookingService) {
     this.flightsToPay = bookingService.flightsToPay;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.invokeStripe();
+  }
+  
+  makePayment(amount) {
+    const paymentHandler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_51JuGPUETqlXqcNbHQoJB3ZzDzeoNENAtAl8zabUCyif8ZLTtPFPq0a2GRCvJYliNSDb6fKzDRBwYng40So42mQP500Cy1ROssB',
+      locale: 'auto',
+      token: function (stripeToken: any) {
+        console.log(stripeToken)
+        alert('Stripe token generated!');
+      }
+    });
+  
+    paymentHandler.open({
+      name: 'Payment',
+      amount: amount * 100
+    });
+  }
+  
+  invokeStripe() {
+    if(!window.document.getElementById('stripe-script')) {
+      const script = window.document.createElement("script");
+      script.id = "stripe-script";
+      script.type = "text/javascript";
+      script.src = "https://checkout.stripe.com/checkout.js";
+      script.onload = () => {
+        this.paymentHandler = (<any>window).StripeCheckout.configure({
+          key: 'pk_test_51JuGPUETqlXqcNbHQoJB3ZzDzeoNENAtAl8zabUCyif8ZLTtPFPq0a2GRCvJYliNSDb6fKzDRBwYng40So42mQP500Cy1ROssB',
+          locale: 'auto',
+          token: function (stripeToken: any) {
+            console.log(stripeToken)
+            alert('Payment has been successfull!');
+          }
+        });
+      }
+        
+      window.document.body.appendChild(script);
+    }
+  }
 }
